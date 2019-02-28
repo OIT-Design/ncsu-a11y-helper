845655fe3a1483c989ef934d8829f03b447de8b5
Brian DeConinck <bpdeconi@ncsu.edu>
2019-02-28 17:36:05 -0500

    Bugfixes

--- inc/info_metabox.php
+++ inc/info_metabox.php
@@ -18,12 +18,34 @@ add_action( 'add_meta_boxes', 'ncsu_a11y_meta_box' );
 
 function ncsu_a11y_meta_content() {
 
+    $query_args = array();
+
+	$query_args['preview_id']    = $post->ID;
+	$query_args['preview_nonce'] = wp_create_nonce( 'post_preview_' . $post->ID );
+	if ( isset( $_POST['post_format'] ) ) {
+		$query_args['post_format'] = empty( $_POST['post_format'] ) ? 'standard' : sanitize_key( $_POST['post_format'] );
+	}
+	if ( isset( $_POST['_thumbnail_id'] ) ) {
+		$query_args['_thumbnail_id'] = ( intval( $_POST['_thumbnail_id'] ) <= 0 ) ? '-1' : intval( $_POST['_thumbnail_id'] );
+	}
+
+	$query_args['ncsu_a11y'] = 'true';
+
+    $button = sprintf(
+        '<a class="button" href="%s" target="wp-preview-%s" id="ncsu-a11y-preview">
+                %s
+            </a>',
+        get_preview_post_link( $post, $query_args ) . '#a11y-report',
+        $post->ID,
+        __( 'Run Accessibility Check<span class="screen-reader-text"> (opens in a new window)</span>', 'ncsu-a11y-helper' )
+        );
+
     $default_meta_text = '<h3>' . __( 'What is Web Accessibility?', 'ncsu-a11y-helper' ) . '</h3>'
                          .'<p><a href="http://go.ncsu.edu/what-is-a11y">' . __( 'Web accessibility', 'ncsu-a11y-helper' ) . '</a> '
                          . __( 'is about ensuring that everyone who visits your website will be able to understand and interact with your website. This especially includes users with disabilities, but good accessibility has benefits to all users. Learn more about ', 'ncsu-a11y-helper' ) . '<a href="http://go.ncsu.edu/a11y-intro">' . __( 'the basics of web accessibility', 'ncsu-a11y-helper' ) . '</a>, <a href="http://go.ncsu.edu/inclusive-design-principles">' . __( 'Inclusive Design Principles', 'ncsu-a11y-helper' ) . '</a>, ' . __( 'and ', 'ncsu-a11y-helper' ) . '<a href="http://go.ncsu.edu/universal-design-principles">' . __( 'Universal Design Principles', 'ncsu-a11y-helper' ) . '</a>.</p>'
                          . '<h3>' . __( 'Testing for Web Accessibility', 'ncsu-a11y-helper' ) . '</h3>'
                          . '<p>' . __( 'Before publishing new content, run the Accessibility Check:', 'ncsu-a11y-helper' ) . '</p>'
-                         . '<a class="button" href="'. get_preview_post_link( $post, array( 'ncsu_a11y' => 'true' ) ) .'" target="wp-preview-1">' . __( 'Run Accessibility Check', 'ncsu-a11y-helper' ) . '<span class="screen-reader-text"> ' . __( '(opens in a new window)', 'ncsu-a11y-helper' ) . '</span></a>'
+                         . $button
                          . '<p>' . __( 'The Accessibility Check will scan your post or page for common accessibility issues. If it detects something that might be an issue, it will highlight that part of your post or page, and provide more information about what the issue is, why it matters, and how you can fix it.', 'ncsu-a11y-helper' ) . '</p>'
                          . '<p>' . __( 'But automated testing can\'t detect everything. As you write and build your post or page, think about potential barriers that would prevent some users from understanding your content. Some easy manual tests you can do include:', 'ncsu-a11y-helper' ) . '</p>'
                          . '<ul>'
--- inc/publish_metabox.php
+++ inc/publish_metabox.php
@@ -1,7 +1,11 @@
 <?php 
 
-// Add "Run Accessibility Scan" button to Publish metabox
+add_action('post_submitbox_misc_actions', 'ncsu_a11y_run_button');
+
+// Add "Run Accessibility Scan" button to Classic Editor Publish metabox
 function ncsu_a11y_run_button($post) {
+
+    $post_id = (int) $post->ID;
 
     $ncsu_a11y_options = get_option( 'ncsu_a11y', array() );
     $current_screen = get_current_screen()->id;
@@ -12,15 +16,38 @@ function ncsu_a11y_run_button($post) {
         $checked_post_types = get_post_types( array( 'public' => true ) );
     }
 
+	$query_args = array();
+
+	$query_args['preview_id']    = $post->ID;
+	$query_args['preview_nonce'] = wp_create_nonce( 'post_preview_' . $post->ID );
+	if ( isset( $_POST['post_format'] ) ) {
+		$query_args['post_format'] = empty( $_POST['post_format'] ) ? 'standard' : sanitize_key( $_POST['post_format'] );
+	}
+	if ( isset( $_POST['_thumbnail_id'] ) ) {
+		$query_args['_thumbnail_id'] = ( intval( $_POST['_thumbnail_id'] ) <= 0 ) ? '-1' : intval( $_POST['_thumbnail_id'] );
+	}
+
+	$query_args['ncsu_a11y'] = 'true';
+    
+
     if ( in_array($current_screen, $checked_post_types) ) {
         echo sprintf(
-            '<div class="misc-pub-section"><a class="preview button" href="%s" target="wp-preview-1" id="ncsu-a11y-preview">%s</a><br /></div><a class="misc-pub-section preview" href="#ncsu_a11y">%s</a>',
-            get_preview_post_link( $post, array( 'ncsu_a11y' => 'true' ) ),
-            'Run Accessibility Check<span class="screen-reader-text"> (opens in a new window)</span>',
-            'Learn more about accessibility'
+            '<div class="misc-pub-section">
+                <a class="preview button" href="%s" target="wp-preview-%s" id="ncsu-a11y-preview">
+                    %s
+                </a>
+                <input type="hidden" name="wp-preview" id="wp-preview" value="">
+                <br />
+            </div>
+            <a class="misc-pub-section preview" href="#ncsu_a11y">
+                %s
+            </a>',
+            get_preview_post_link( $post, $query_args ) . '#a11y-report',
+            $post->ID,
+            __( 'Run Accessibility Check<span class="screen-reader-text"> (opens in a new window)</span>', 'ncsu-a11y-helper' ),
+            __( 'Learn more about accessibility', 'ncsu-a11y-helper' )
             );
         
     }
 
 }
-add_action('post_submitbox_misc_actions', 'ncsu_a11y_run_button');
